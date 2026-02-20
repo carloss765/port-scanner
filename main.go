bd2f67aed3b75f328fb1f235be290be0701ec476
carloss765 <carlosandresmartinezortiz632@gmail.com>
2026-02-20 14:37:57 -0500

    feat: Implement concurrent port scanning with a worker pool, channels, and command-line flags for host, port range, and worker count.

--- main.go
+++ main.go
@@ -1,32 +1,64 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net" // Managen network connections
 	"sync"
 	"time"
 )
 
-func scanPort(wg *sync.WaitGroup, host string, port int) {
+func worker(wg *sync.WaitGroup, ports <-chan int, host string, results chan<- int) {
 	defer wg.Done()
 
-	address := fmt.Sprintf("%s:%d", host, port)
-	conn, err := net.DialTimeout("tcp", address, 500*time.Millisecond) // try connect to remote service
-	if err != nil {
-		fmt.Printf("Puerto %d cerrado o filtrado (%v)\n", port, err) //? fails optional for user
-		return
+	for port := range ports {
+		address := fmt.Sprintf("%s:%d", host, port)
+		conn, err := net.DialTimeout("tcp", address, 500*time.Millisecond) // try connect to remote service
+		if err == nil {
+			conn.Close()
+			results <- port
+		}
 	}
-	conn.Close()
-	fmt.Printf("Puerto %d abierto\n", port)
 }
 
 func main() {
-	host := "scanme.nmap.org"
+	host := flag.String("host", "127.0.0.1", "Host to scan") // flags for agility in terminal
+	start := flag.Int("start", 1, "Puerto inicial")
+	end := flag.Int("end", 1024, "Puerto final")
+	workers := flag.Int("workers", 100, "Cantidad de workers")
+
+	flag.Parse()
+
+	startTime := time.Now()
+
+	ports := make(chan int, *workers)
+	results := make(chan int)
+
 	var wg sync.WaitGroup
 
-	for port := 1; port <= 100; port++ {
+	for i := 1; i <= *workers; i++ { // create workers
 		wg.Add(1)
-		go scanPort(&wg, host, port) // gorutine
+		go worker(&wg, ports, *host, results)
 	}
-	wg.Wait()
+
+	go func() { // scann ports
+		for port := *start; port <= *end; port++ {
+			ports <- port
+		}
+		close(ports)
+	}()
+
+	go func() { // close results when all workers are done
+		wg.Wait()
+		close(results)
+	}()
+
+	fmt.Printf("Escaneando...")
+
+	for port := range results {
+		fmt.Printf("Puerto %d esta abierto\n", port)
+	}
+
+	elapsed := time.Since(startTime)
+	fmt.Printf("Tiempo total: %s\n", elapsed)
 }
